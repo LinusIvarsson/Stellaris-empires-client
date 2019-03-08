cb3e1422f77556f82b5fa6ceb57c3f563c0d467a
Linus Ivarsson <ivarsson.linus@gmail.com>
2019-03-08 20:45:24 +0100

    Implemented logic for validating governments

--- src/app/components/government/authority/authority.component.ts
+++ src/app/components/government/authority/authority.component.ts
@@ -25,6 +25,7 @@ export class AuthorityComponent implements OnInit, DoCheck {
 
   ngDoCheck() {
     if (this.activeEthicLength !== this.activeEthics.length) {
+      this.activeEthicLength = this.activeEthics.length;
       this.validateAuthorities();
     }
   }
@@ -32,12 +33,45 @@ export class AuthorityComponent implements OnInit, DoCheck {
   click() {}
 
   validateAuthorities() {
-    for (let authority of this.authorities) {
+    for (const authority of this.authorities) {
       switch (authority.name) {
         case 'Democratic': {
           this.validateDemocratic(authority);
+          break;
+        }
+        case 'Oligarchic': {
+          this.validateOligarchic(authority);
+          break;
+        }
+        case 'Dictatorial': {
+          this.validateDictatorial(authority);
+          break;
+        }
+        case 'Imperial': {
+          this.validateImperial(authority);
+          break;
+        }
+        case 'Hive Mind': {
+          this.validateHiveMind(authority);
+          break;
+        }
+        case 'Machine Intelligence': {
+          this.validateMachineIntelligence(authority);
+          break;
+        }
+        case 'Corporate': {
+          this.validateCorporate(authority);
+          break;
         }
       }
+    }
+  }
+
+  setStatus(index: number, authority: IAuthority) {
+    if (index > -1) {
+      authority.status = AuthorityStatus.disabled;
+    } else {
+      authority.status = AuthorityStatus.available;
     }
   }
 
@@ -45,14 +79,78 @@ export class AuthorityComponent implements OnInit, DoCheck {
     const hasConflictingEthic = this.activeEthics.findIndex(
       auth =>
         auth.name === 'authoritarian' ||
-        auth.name === 'fanatic authoritarian' ||
-        auth.name === 'gestalt consciousness'
+        auth.name === 'fanatic_authoritarian' ||
+        auth.name === 'gestalt_consciousness'
     );
 
-    if (hasConflictingEthic > -1) {
-      authority.status = AuthorityStatus.disabled;
-    } else {
+    this.setStatus(hasConflictingEthic, authority);
+  }
+
+  validateOligarchic(authority: IAuthority) {
+    const hasConflictingEthic = this.activeEthics.findIndex(
+      auth =>
+        auth.name === 'fanatic_eqalitarian' ||
+        auth.name === 'fanatic_authoritarian' ||
+        auth.name === 'gestalt_consciousness'
+    );
+
+    this.setStatus(hasConflictingEthic, authority);
+  }
+
+  validateDictatorial(authority: IAuthority) {
+    const hasConflictingEthic = this.activeEthics.findIndex(
+      auth =>
+        auth.name === 'eqalitarian' ||
+        auth.name === 'fanatic_eqalitarian' ||
+        auth.name === 'gestalt_consciousness'
+    );
+
+    this.setStatus(hasConflictingEthic, authority);
+  }
+
+  validateImperial(authority: IAuthority) {
+    const hasConflictingEthic = this.activeEthics.findIndex(
+      auth =>
+        auth.name === 'eqalitarian' ||
+        auth.name === 'fanatic_eqalitarian' ||
+        auth.name === 'gestalt_consciousness'
+    );
+
+    this.setStatus(hasConflictingEthic, authority);
+  }
+
+  validateCorporate(authority: IAuthority) {
+    const hasConflictingEthic = this.activeEthics.findIndex(
+      auth =>
+        auth.name === 'fanatic_authoritarian' ||
+        auth.name === 'fanatic_eqalitarian' ||
+        auth.name === 'gestalt_consciousness'
+    );
+
+    this.setStatus(hasConflictingEthic, authority);
+  }
+
+  validateHiveMind(authority: IAuthority) {
+    const hasGestaltConsciousness = this.activeEthics.findIndex(
+      auth => auth.name === 'gestalt_consciousness'
+    );
+
+    if (hasGestaltConsciousness > -1 || this.activeEthicLength === 0) {
       authority.status = AuthorityStatus.available;
+    } else {
+      authority.status = AuthorityStatus.disabled;
+    }
+  }
+
+  validateMachineIntelligence(authority: IAuthority) {
+    const hasGestaltConsciousness = this.activeEthics.findIndex(
+      auth => auth.name === 'gestalt_consciousness'
+    );
+
+    if (hasGestaltConsciousness > -1 || this.activeEthicLength === 0) {
+      authority.status = AuthorityStatus.available;
+    } else {
+      authority.status = AuthorityStatus.disabled;
     }
   }
 }
