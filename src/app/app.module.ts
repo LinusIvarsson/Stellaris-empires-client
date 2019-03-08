b0ebbcc52cca02b0d7b6de258b1ae5a67f39823e
Linus Ivarsson <linus.ivarsson@sigma.se>
2019-03-08 11:02:53 +0100

    Added authority component, refactored folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EthicsComponent } from './components/ethics/ethics.component';
+import { EthicsComponent } from './components/government/ethics/ethics.component';
+import { GovernmentComponent } from './components/government/government.component';
+import { AuthorityComponent } from './components/government/authority/authority.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EthicsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EthicsComponent, GovernmentComponent, AuthorityComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
