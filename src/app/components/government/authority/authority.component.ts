b6a86983f94b78ec15c9c9694af2f09e4db43f17
Linus Ivarsson <linus.ivarsson@sigma.se>
2019-03-08 16:43:54 +0100

    WIP government validation

--- src/app/components/government/authority/authority.component.ts
+++ src/app/components/government/authority/authority.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, DoCheck } from '@angular/core';
 import { IEthic } from 'src/app/core/models/IEthic';
 import { IAuthority } from 'src/app/core/models/IAuthority';
 import {
@@ -11,15 +11,48 @@ import {
   templateUrl: './authority.component.html',
   styleUrls: ['./authority.component.scss']
 })
-export class AuthorityComponent implements OnInit {
+export class AuthorityComponent implements OnInit, DoCheck {
   @Input() activeEthics: IEthic[];
   authorities: IAuthority[] = [];
   authorityStatus = AuthorityStatus;
+  activeEthicLength = 0;
   constructor() {}
 
   ngOnInit() {
     this.authorities = AuthorityUtils.getAuthorities();
+    this.activeEthicLength = this.activeEthics.length;
+  }
+
+  ngDoCheck() {
+    if (this.activeEthicLength !== this.activeEthics.length) {
+      this.validateAuthorities();
+    }
   }
 
   click() {}
+
+  validateAuthorities() {
+    for (let authority of this.authorities) {
+      switch (authority.name) {
+        case 'Democratic': {
+          this.validateDemocratic(authority);
+        }
+      }
+    }
+  }
+
+  validateDemocratic(authority: IAuthority) {
+    const hasConflictingEthic = this.activeEthics.findIndex(
+      auth =>
+        auth.name === 'authoritarian' ||
+        auth.name === 'fanatic authoritarian' ||
+        auth.name === 'gestalt consciousness'
+    );
+
+    if (hasConflictingEthic > -1) {
+      authority.status = AuthorityStatus.disabled;
+    } else {
+      authority.status = AuthorityStatus.available;
+    }
+  }
 }
