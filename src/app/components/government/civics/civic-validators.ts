55f7bcb492f338ca274f8f4cb6e1d60f01985068
Linus Ivarsson <ivarsson.linus@gmail.com>
2019-03-11 07:06:31 +0100

    Made civics unselectable, small fix in return value of validators.

--- src/app/components/government/civics/civic-validators.ts
+++ src/app/components/government/civics/civic-validators.ts
@@ -7,7 +7,7 @@ export class CivicValidators {
       ethic => ethic.name === 'pacifist' || ethic.name === 'fanatic_pacifist'
     );
 
-    return !hasRequiredEthic;
+    return !!~hasRequiredEthic;
   }
 
   static validateAristocraticElite(
